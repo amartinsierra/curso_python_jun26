a450f3d89492043b0511f9235d056e6e6a76285e
amartinsierra <ajms66@gmail.com>
2026-06-17 17:35:19 +0200

    proyecto empleados completo

--- 18_proyecto_empleados.py
+++ 18_proyecto_empleados.py
@@ -10,19 +10,24 @@ empleados=[
 ]
 #indica si existe un empleado a partir del nombre recibido
 def existeEmpleado(nombre):
-    
+    return nombre in list(map(lambda e:e["nombre"],empleados))
+
 #devuelve una lista con los empleados del departamento recibido
 def empleadosPorDepartamento(dep):
-    
+    return list(filter(lambda e:e["departamento"]==dep,empleados)),
 #devuelve una lista con los nombres de los departamentos
 def departamentos():
-    
+    return set(map(lambda e:e["departamento"],empleados))
 #devuelve la media de salarios
 def salarioMedio():
-    
+    salarios=list(map(lambda e:e["salario"],empleados))
+    return sum(salarios)/len(salarios)
 #devuelve la media de edad del departamento recibido
 def mediaEdadDepartamento(departamento):
-    
+    edades=list(map(lambda e:e["edad"],list(filter(lambda e:e["departamento"]==departamento,empleados))))
+    return sum(edades)/len(edades)
 #devuelve la lista de empleados, ordenados por salario
 def ordenarPorSalario():
+   return list(map(lambda e:(e["nombre"],e["salario"]),sorted(empleados,key=lambda e:e["salario"])))
     
+print(ordenarPorSalario())   
